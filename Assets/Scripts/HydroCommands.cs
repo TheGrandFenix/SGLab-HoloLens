8655f5cfeee5e2bbd37fb091430ad715e9898900
Fran Jurinec <franjurinec@live.com>
2019-03-10 18:50:11 +0100

    Input Testing

---
+++ Assets/Scripts/HydroCommands.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HydroCommands : MonoBehaviour
+{
+    void OnTapped()
+    {
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+}
